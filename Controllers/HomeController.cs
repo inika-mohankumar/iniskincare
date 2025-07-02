98efb587b3352f51d9a022ea993b41f051be9a71
inika-mohankumar <mohaninika@gmail.com>
2025-07-02 13:35:13 +0530

    productcontroller changes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,13 +9,12 @@ namespace iniskincare.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly IniskincareDbContext _context;
+        //private readonly IniskincareDbContext _context;
 
-        public HomeController(IniskincareDbContext context)
-        {
-            _context = context;
-        }
-
+        //public HomeController(IniskincareDbContext context)
+        //{
+        //    _context = context;
+        //}
         public IActionResult Index()
         {
             string email = HttpContext.Session.GetString("UserEmail");
@@ -33,44 +32,6 @@ namespace iniskincare.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
-        public IActionResult Products(string searchTerm)
-        {
-            List<string> categories = new List<string>
-            {
-              "Body Care",
-              "Face Care",
-              "Lip Care",
-              "Hair Care",
-              "Sun Protection",
-              "All"
-            };
-            ViewBag.Categories = categories;
-
-            var allProducts = _context.Product.ToList();
-
-            if (!string.IsNullOrEmpty(searchTerm))
-            {
-                searchTerm = searchTerm.ToLower();
-                allProducts = allProducts
-                    .Where(p => p.Name.ToLower().Contains(searchTerm))
-                    .ToList();
-            }
-
-            return View(allProducts);
-        }
-
-        [HttpGet]
-        public IActionResult Details(int id)
-        {
-            var product = _context.Product.FirstOrDefault(p => p.Id == id);
-            if (product == null)
-            {
-                return NotFound();
-            }
-
-            return View(product);
         }
 
         public IActionResult About() => View();
